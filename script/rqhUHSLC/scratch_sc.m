465624740a0729086b68e833c189d40d56990e3c
Benjamin Huang <bdhuang@princeton.edu>
2017-08-07 09:34:26 -0400

    Updated to find highest density block of time that I have data for

--- script/rqhUHSLC/scratch_sc.m
+++ script/rqhUHSLC/scratch_sc.m
@@ -1,8 +1,16 @@
 %% Load the data
 [time,ssh,loc] = saveRange;
 
-%% 
-
+%% Find the highest density area
+% Put all times in one array
+timeT = cellfun(@(x) x', time,'UniformOutput',0);
+allTime = [timeT{:}];
+% Look at histcounts
+[nums,edges] = histcounts(allTime);
+% Find highest density 
+[val,ind] = max(nums);
+edg1 = edges(ind);
+edg2 = edges(ind + 1);
 
 %% Plot the Data
 for k=1:length(all)
