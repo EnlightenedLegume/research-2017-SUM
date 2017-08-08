19d151a653af550c446bb1ee9f584906f7724998
Benjamin Huang <bdhuang@princeton.edu>
2017-08-08 14:20:37 -0400

    Bug fixes 1) Fixed issue with URL generation     a) Discovery that there was a second standard of URLs caused need for         an update     b) Added checks to discover which standard was being used     c) Slight changes to the way the blacklist file is written     d) URL checks are now done using curl with the --head flag to         reduce how much data need to be downloaded

--- script/pullData/getRQHTide.sh
+++ script/pullData/getRQHTide.sh
@@ -10,23 +10,55 @@
 
 # Loop through ID file
 while read p; do
-
+    # Tell me what's getting checked 
+    echo "Checking station $p"
     # The basic URL (RQH -> Research Quality, Hourly)
     urlBase="http://uhslc.soest.hawaii.edu/thredds/dodsC/uhslc/rqh/OS_UH-RQH"
     togaId=$p
-    urlEnd="_20160323_D"
     
-    # Tell me what file is being attempted
-    echo "Checking $togaId"
-    # First, download the DDS 
-    urlDDS=$urlBase$togaId$urlEnd".dds"
-    wget $urlDDS -qO DDS.txt
+    # Two standards appear to exist, _D and _R,
+    # so we need to figure out which one is being used
+    urlEnd_D="_20160323_D"
+    urlEnd_R="_20160323_R"
+    
+    # First, download the DDS's
+    urlDDS_D=$urlBase$togaId$urlEnd_D".dds"
+    urlDDS_R=$urlBase$togaId$urlEnd_R".dds"
+    # Check both stations and save the results to variables
+    curl -s --head $urlDDS_D | head -n 1 | grep "HTTP/1.[01] [23].."
+    resp_D=$?
+    curl -s --head $urlDDS_R | head -n 1 | grep "HTTP/1.[01] [23].."
+    resp_R=$?
 
-    # Check if response was succesful, otherwise blacklist id
-    if [ $? -eq 0 ]
-    then 
+    # Figure out which one to do
+    # Create boolean for downloading, default to true
+    exists=0
+    if [ $resp_D -eq 0 ]; then
+	urlEnd=$urlEnd_D
+    elif [ $resp_R -eq 0 ]; then
+	urlEnd=$urlEnd_R
+    else 
+	# Set exists boolean to skip download step
+	exists=1
+	# Blacklist ID if both fail!
+	if [ -a blacklist.txt ]; then
+	    # Append if the file already exists
+	    echo $togaId >> blacklist.txt
+	else
+	    # Otherwise, create the blacklist file
+	    echo $togaId > blacklist.txt
+	fi
+    fi
+    
+    # Actual downloading and parsing section
+    if [ $exists -eq 0 ]; then
+	urlDDS=$urlBase$togaId$urlEnd".dds"
+	# Tell me what station is being checked
+	echo "Checking station $togaId with DDS URL $urlDDS"
+	# Get the actual DDS, not just the head
+	wget $urlDDS -qO DDS.txt
 	# Parse the number of time points
-	time="$(awk '/Float64 time\[time = [1-9]*/ { print $4}' DDS.txt | awk -F']' '/[1-9]*/ {print $1; exit}' )"
+	time="$(awk '/Float64 time\[time = [1-9]*/ {print $4}' DDS.txt | awk -F']' '/[1-9]*/ {print $1; exit}')"
 	# Subtract 1 for 0 indexing
 	time="$((time -1))"
 
@@ -36,15 +68,6 @@ while read p; do
 	fname=$togaId".txt"
 	# Get data and save
 	wget $urlASCII -O $2$fname
-    else 
-	# Blacklist id, but check if blacklist file exists
-	if [ -a blacklist.txt ]
-	then
-	    echo $togaId >> blacklist.txt
-	else
-	    # Otherwise, create the blacklist file
-	    echo $togaId > blacklist.txt
-	fi
     fi
 done < $1
 
